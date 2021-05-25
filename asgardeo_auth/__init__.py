3eb727e53f19770e827a6358e39e5400d2a30941
Sathiyakugan <sathiyakugan.15@cse.mrt.ac.lk>
2021-05-25 12:10:50 +0530

    Add changes to the package folder name

---
+++ asgardeo_auth/__init__.py
@@ -0,0 +1,21 @@
+from .asgardeo_auth import AsgardeoAuth
+from .constants.common import *
+from .constants.user import *
+from .exception.asgardeo_auth_error import AsgardeoAuthError
+
+from asgardeo_auth.app_consts import version, homepage, author, \
+    license_name, name, packages, description, author_email, download_url, \
+    keywords, bug_tracker
+
+__version__ = version
+__name__ = name
+__packages__ = packages
+__homepage__ = homepage
+__author__ = author
+__author_email__ = author_email
+__download_url__ = download_url
+__keywords__ = keywords
+__license_name__ = license_name
+__description__ = description
+__bug_tracker__ = bug_tracker
+
